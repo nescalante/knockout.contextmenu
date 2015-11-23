95b37e889917065f9c0e5d1364ab424bb0176248
Nicolas Escalante <nlante@gmail.com>
2015-11-23 14:23:15 -0300

    release version 0.3.6

--- dist/js/knockout.contextmenu.js
+++ dist/js/knockout.contextmenu.js
@@ -1,4 +1,4 @@
-/* knockout.contextmenu v0.3.5
+/* knockout.contextmenu v0.3.6
    Nicolás Escalante - nlante@gmail.com
    Issues: https://github.com/nescalante/knockout.contextmenu/issues
    License: MIT */
@@ -100,37 +100,39 @@ function bindContextMenu(ko, document) {
 
         hideCurrentMenu();
 
-        // make visibility hidden, then add to DOM so that we can get the height/width of the menu
-        menuElement.style.visibility = 'hidden';
-        (document.body || document).appendChild(menuElement);
+        if (menuElement) {
+          // make visibility hidden, then add to DOM so that we can get the height/width of the menu
+          menuElement.style.visibility = 'hidden';
+          (document.body || document).appendChild(menuElement);
 
-        // set location
-        if (event) {
-          var bottomOfViewport = window.innerHeight + window.pageYOffset;
-          var rightOfViewport = window.innerWidth + window.pageXOffset;
+          // set location
+          if (event) {
+            var bottomOfViewport = window.innerHeight + window.pageYOffset;
+            var rightOfViewport = window.innerWidth + window.pageXOffset;
 
-          if (mouseY(event) + menuElement.offsetHeight > bottomOfViewport) {
-            menuElement.style.top = 1 * (bottomOfViewport - menuElement.offsetHeight - 10) + 'px';
-          } else {
-            menuElement.style.top = mouseY(event) + 'px';
+            if (mouseY(event) + menuElement.offsetHeight > bottomOfViewport) {
+              menuElement.style.top = 1 * (bottomOfViewport - menuElement.offsetHeight - 10) + 'px';
+            } else {
+              menuElement.style.top = mouseY(event) + 'px';
+            }
+
+            if (mouseX(event) + menuElement.offsetWidth > rightOfViewport) {
+              menuElement.style.left = 1 * (rightOfViewport - menuElement.offsetWidth - 10) + 'px';
+            } else {
+              menuElement.style.left = mouseX(event) + 'px';
+            }
+
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          else {
+            menuElement.style.top = (element.offsetTop + element.offsetHeight) + 'px';
+            menuElement.style.left = (element.offsetLeft + element.offsetWidth) + 'px';
           }
 
-          if (mouseX(event) + menuElement.offsetWidth > rightOfViewport) {
-            menuElement.style.left = 1 * (rightOfViewport - menuElement.offsetWidth - 10) + 'px';
-          } else {
-            menuElement.style.left = mouseX(event) + 'px';
-          }
-
-          event.preventDefault();
-          event.stopPropagation();
+          // now set to visible
+          menuElement.style.visibility = '';
         }
-        else {
-          menuElement.style.top = (element.offsetTop + element.offsetHeight) + 'px';
-          menuElement.style.left = (element.offsetLeft + element.offsetWidth) + 'px';
-        }
-
-        // now set to visible
-        menuElement.style.visibility = '';
 
         // replace current menu with the recently created
         currentMenu = menuElement;
