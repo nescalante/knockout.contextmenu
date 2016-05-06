3b7f1ff517593903cd02b1c28150400262666248
Nicolas Escalante <nlante@gmail.com>
2016-05-06 17:53:16 -0300

    add files when versioning

--- dist/js/knockout.contextmenu.min.js
+++ dist/js/knockout.contextmenu.min.js
@@ -1,4 +1,4 @@
-/* knockout.contextmenu v0.4.2
+/* knockout.contextmenu v0.4.3
    Nicolás Escalante - nlante@gmail.com
    Issues: https://github.com/nescalante/knockout.contextmenu/issues
    License: MIT */
